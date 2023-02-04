c68dbc310c5daa492bf9511cff579205d486a28e
Unknown_69 <88512161+KnownUnknown420@users.noreply.github.com>
2023-02-04 00:06:22 -0800

    Create Main.js

---
+++ Tower Defense/Main.js
@@ -0,0 +1,12 @@
+//code for stuff
+const Xmax = 600; const Ymax = 600;
+
+//mainly for testing
+function setup() {
+  createCanvas(Xmax,Ymax);
+  CreateWave(5, 300 ,400-15,-30,30,30)
+}
+
+function draw(){
+  DrawMap();
+}
