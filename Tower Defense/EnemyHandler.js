317e6eb9019590406d0683089c719121b76262e6
Unknown_69 <88512161+KnownUnknown420@users.noreply.github.com>
2023-02-28 22:51:27 -0800

    Update EnemyHandler.js

--- Tower Defense/EnemyHandler.js
+++ Tower Defense/EnemyHandler.js
@@ -1,73 +1,162 @@
 //array with all current enemies on screen
-let Enemies = []
+let Enemies = [];
+let SpawnCheck = false;
 
 //points that the enemies have to follow
 let PathPoints = [
-  385,110, 
-  150,110, 
-  150,360,
-  400,360,
-  400,460,
-  200,460,
-  200,600
-]
+  385,
+  110,
+  150,
+  110,
+  150,
+  360,
+  400,
+  360,
+  400,
+  460,
+  200,
+  460,
+  200,
+  600,
+];
 
-//enemy class
-class Enemy {
-  
-  //builds the enemy
-  constructor(x, y, h, speed) {
-    this.x = x;
-    this.y = y;
-    this.h = h;
-    this.speed = speed;
-    this.pathPoints = PathPoints;
-    this.currentPoint = 0;
-    this.completed = false;
-    this.health = 1
+//Shows the enemies on the canvas
+function ShowEnemies() {
+  for (let i = 0; i < Enemies.length; i++) {
+    if (Enemies[i].Moving == true) {
+      Enemies[i].show();
+      Enemies[i].move();
+    }
   }
-  
-  //sets up move function, loops automaticly
+}
+
+//Enemy class
+class Enemy {
+  //Builds an enemy with initial position and speed
+  constructor() {
+    this.h = 30; // Height of the enemy square
+    this.x = MapData[6] - (this.h / 2); // X-coordinate of the enemy
+    this.y = MapData[7] - this.h; // Y-coordinate of the enemy
+    this.speed = 30; // Speed of the enemy
+    this.pathPoints = PathPoints; // Path that the enemy has to follow
+    this.Moving = false;
+    this.currentPoint = 0; // Current point on the path that the enemy is at
+    this.health = 1; // Health of the enemy
+    this.orginalHealth = this.health;
+    this.resistance = {}
+  }
+
+  //Moves the enemy along the path
   move() {
-  let interval = setInterval(() => {
+    //Coordinates of the target point on the path
     let targetX = this.pathPoints[this.currentPoint * 2];
     let targetY = this.pathPoints[this.currentPoint * 2 + 1];
 
+    //Calculates the difference between the enemy's current position and the target point
     let dx = targetX - this.x;
     let dy = targetY - this.y;
     let distance = sqrt(dx * dx + dy * dy);
 
+    //Moves the enemy closer to the target point
     this.x += (dx * this.speed) / (distance * 10);
     this.y += (dy * this.speed) / (distance * 10);
-    //console.log(distance)
-    if (floor(distance) <= 1 + this.speed / 5){
+
+    //If the enemy has reached the target point
+    if (floor(distance) <= 1 + this.speed / 5) {
       this.x = targetX;
       this.y = targetY;
       this.currentPoint++;
 
+      //If the enemy has reached the end of the path
       if (this.currentPoint >= this.pathPoints.length / 2) {
-        this.completed = true
-        GameHealth -= this.health
+        let index = Enemies.indexOf(this);
+        //Removes the enemy from the enemies array
+        Enemies.splice(index, 1);
+        GameHealth -= floor(this.health);
       }
     }
-    }, 1000/60);
   }
-  
-  //shows the block
-  show(){
-  fill(255);
-  square(this.x, this.y, this.h);
+
+  //Displays the enemy on the canvas
+  show() {
+    fill(255);
+    square(this.x, this.y, this.h);
+
+    if (this.health != this.orginalHealth) {
+      fill(50, 50, 50, 120);
+      rect(this.x - 13, this.y - 13, this.h + 26, 7);
+
+      fill(255, 0, 0, 200); //health bar
+      rect(
+        this.x - 13,
+        this.y - 13,
+        (this.health / this.orginalHealth) * (this.h + 26),
+        5
+      );
+    }
   }
 }
 
-
-//code to create a wave
-function CreateWave(Amount, Delay, x, y, h, speed) {
-  for (let i = 0; i < Amount; i++) {
-    let enemy = new Enemy(x, y, h, speed);
-    setTimeout(() => {
-      enemy.move();
-    }, i * Delay);
-    Enemies.push(enemy);
+class Soilder extends Enemy {
+  constructor(Health) {
+    super();
+    this.health = Health;
+    this.orginalHealth = Health;
+    this.speed = 30;
+    this.h = 30;
   }
 }
+
+class Camo extends Enemy {
+  constructor(Health) {
+    super();
+    this.health = Health;
+    this.orginalHealth = Health;
+    this.speed = 30;
+    this.h = 30;
+    this.resistance = {camo}
+  }
+}
+
+class Tank extends Enemy {
+  constructor(Health) {
+    super();
+    this.health = Health;
+    this.orginalHealth = Health;
+    this.speed = 30;
+    this.h = 30;
+    this.resistance = {tank}
+  }
+}
+
+class CamoTank extends Enemy {
+  constructor(Health) {
+    super();
+    this.health = Health;
+    this.orginalHealth = Health;
+    this.speed = 30;
+    this.h = 30;
+    this.resistance = {camo,tank}
+  }
+}
+
+function SpawnEnemies(Type, Amount, Delay, Debounce, Health) {
+  WaveStarted = true;
+  setTimeout(() => {
+    if (SpawnCheck === false) {
+      SpawnCheck = true;
+      for (let i = 0; i < Amount; i++) {
+        // create a new enemy instance
+        let enemy = new Type(Health);
+        // set a timeout to start moving the enemy after the specified delay
+        setTimeout(() => {
+          enemy.Moving = true;
+        }, i * Debounce);
+        // add the enemy to the array of all current enemies
+        Enemies.push(enemy);
+      }
+    }
+  }, Delay);
+}
+
+SpawnEnemies(Soilder, 100, 5000, 500, 5);
