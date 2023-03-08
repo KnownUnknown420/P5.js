580df3d71d8d24a9d9e789cb72a0a37a5890d072
Unknown <88512161+KnownUnknown420@users.noreply.github.com>
2023-03-08 12:50:42 -0800

    Update Utilties.js

--- Tower Defense/Utilties.js
+++ Tower Defense/Utilties.js
@@ -27,5 +27,18 @@ function PauseGame() {
   }
 }
 
+function Center(size){
+  return size/2
+}
 
-
+function checkRectIntersection(rect1, rect2) {
+  if (
+    rect1.x < rect2.x + rect2.w &&
+    rect1.x + rect1.w > rect2.x &&
+    rect1.y < rect2.y + rect2.h &&
+    rect1.h + rect1.y > rect2.y
+  ) {
+    return true;
+  }
+  return false;
+}
