a6029f29c78c488d363a12452b6b34643c92ff58
Unknown <88512161+KnownUnknown420@users.noreply.github.com>
2023-03-22 12:28:41 -0700

    Update Main.js

--- Tower Defense/Main.js
+++ Tower Defense/Main.js
@@ -1,62 +1,67 @@
-// Global variables
+//This file contains the games main loop
+//Instead of having each file on there own clock
+//All functions are called here to make sure eveything is insync
+//Can be refered to as GameSync
+
+//Global variables
 const Xmax = 600;
 const Ymax = 600;
 const ShopSize = 200;
-
 let GameHealth = 100;
-let GameMoney = 10000;
-
-let WaveCount = 1;
+let GameMoney = 450; //Defualt 450
 let WaveReward = 100;
+let GamePaused = true;
 
-let GamePaused = false;
-
-// Main loop that refreshes the screen every iteration
+//Standard draw function that runs all the code and stuffs
+//A few Color and drawling fixes are applied after certin function calls
 function draw() {
-  if (focused) {
-    // Draw the background
-    background(MapData[0], MapData[1], MapData[2]);
+  // Draw the background
+  background(MapData[0], MapData[1], MapData[2]);
 
-    // Reset to Track Color
-    fill(MapData[3], MapData[4], MapData[5]);
+  // Reset to Track Color
+  fill(MapData[3], MapData[4], MapData[5]);
 
-    // Draw Track
-    strokeWeight(0);
-    DrawTrack();
-    strokeWeight(1);
+  // Draw Track
+  strokeWeight(0);
+  DrawTrack();
+  strokeWeight(1);
 
-    //SpawnEnemies
-    SpawnEnemiesInWaveQuery();
+  //SpawnEnemies
+  SpawnEnemiesInEnemyMakerInstances();
 
-    // Draw All Active Enemies
-    ShowEnemies();
+  // Draw All Active Enemies
+  ShowEnemies();
 
-    // Draw All Placed Towers
-    ShowTowers();
+  // Draw All Placed Towers
+  ShowTowers();
 
-    fill(0, 0, 0);
-    textSize(20);
+  fill(0, 0, 0);
+  textSize(20);
 
-    // Draw Image Elements
-    DrawGameImages();
+  // Draw Image Elements
+  DrawGameImages();
 
-    // Draw Shop
-    CreateShop();
+  // Draw Shop
+  CreateShop();
 
-    // Draw Text Elements
-    DrawGameText();
-    
-    //Shows all bullets\
-    ShowAttacks();
-  }
+  // Draw Text Elements
+  DrawGameText();
+
+  //Shows all bullets\
+  ShowAttacks();
+
+  //Set waves thingy
+  GenerateWaves();
 }
 
 // Main setup function, mainly used for testing
 function setup() {
   createCanvas(Xmax + ShopSize, Ymax);
-  keyPressed();
+  GenerateWaves();
 }
 
+//Also have key detection here,
+//This is jsut a fast way to check for singe press key clicks
 function keyPressed() {
   if (keyCode === 32) {
     PauseGame();
