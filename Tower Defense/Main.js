2884d3a2710c261b642c233020729ef88336afab
Unknown <88512161+KnownUnknown420@users.noreply.github.com>
2023-04-05 09:28:08 -0700

    Update Main.js

--- Tower Defense/Main.js
+++ Tower Defense/Main.js
@@ -4,60 +4,104 @@
 //Can be refered to as GameSync
 
 //Global variables
+let GameMoney;
 const Xmax = 600;
 const Ymax = 600;
 const ShopSize = 200;
 let GameHealth = 100;
-let GameMoney = 450; //Defualt 450
+let StartMoney = 450; //Defualt 450
 let WaveReward = 100;
 let GamePaused = true;
 
 //Standard draw function that runs all the code and stuffs
 //A few Color and drawling fixes are applied after certin function calls
 function draw() {
-  // Draw the background
-  background(MapData[0], MapData[1], MapData[2]);
+  if (DrawingMenu == true) {
+    if (MainMenu) {
+      textSize(30);
+      text("Squares and Circles", Ymax / 2 - 30, 150);
+      textSize(20);
+      text("A tower defence game", Ymax / 2, 200);
+    }
+    //checks to see if the menu is visible
+    //If it is, we dont need to draw the game
+    DrawMenu();
+  } else {
+    //Otherwise we run the main game loop
+    textSize(15);
+    // Draw the background
+    background(MapData[0], MapData[1], MapData[2]);
 
-  // Reset to Track Color
-  fill(MapData[3], MapData[4], MapData[5]);
+    // Reset to Track Color
+    fill(MapData[3], MapData[4], MapData[5]);
 
-  // Draw Track
-  strokeWeight(0);
-  DrawTrack();
-  strokeWeight(1);
+    // Draw Track
+    strokeWeight(0);
+    DrawTrack();
+    strokeWeight(1);
 
-  //SpawnEnemies
-  SpawnEnemiesInEnemyMakerInstances();
+    //SpawnEnemies
+    SpawnEnemiesInEnemyMakerInstances();
 
-  // Draw All Active Enemies
-  ShowEnemies();
+    if (GameHealth > 0) {
+      // Draw All Active Enemies
+      ShowEnemies();
 
-  // Draw All Placed Towers
-  ShowTowers();
+      // Draw All Placed Towers
+      ShowTowers();
+    }
 
-  fill(0, 0, 0);
-  textSize(20);
+    fill(0, 0, 0);
+    textSize(20);
 
-  // Draw Image Elements
-  DrawGameImages();
+    // Draw Image Elements
+    DrawGameImages();
 
-  // Draw Shop
-  CreateShop();
+    // Draw Shop
+    CreateShop();
 
-  // Draw Text Elements
-  DrawGameText();
+    // Draw Text Elements
+    DrawGameText();
 
-  //Shows all bullets\
-  ShowAttacks();
+    //Shows all bullets\
+    ShowAttacks();
 
-  //Set waves thingy
-  GenerateWaves();
+    //Set waves thingy
+    GenerateWaves();
+
+    //Checks if pause menu is enabled and draws it
+    PauseMenu();
+
+    //Loose screen
+    LooseGamePopUp();
+
+    //WinScreen
+    WinGamePopUp();
+
+    if (GameHealth < 0) {
+      GameHealth = 0;
+    }
+  }
 }
 
-// Main setup function, mainly used for testing
+//setup just used for the menu
 function setup() {
   createCanvas(Xmax + ShopSize, Ymax);
-  GenerateWaves();
+}
+
+//Small function to reset game
+//like the wave and game money
+function ResetGame() {
+  GameHealth = 100;
+  WaveReward = 100;
+  GamePaused = true;
+  GameRestarting = true;
+  WaveIndex = 0;
+  WaveCount = 1;
+  EnemyMakerInstances = [];
+  Enemies = [];
+  PlacedTowers = [];
+  GameMoney = StartMoney;
 }
 
 //Also have key detection here,
@@ -65,5 +109,14 @@ function setup() {
 function keyPressed() {
   if (keyCode === 32) {
     PauseGame();
+  }
+  if (keyCode === 27 && DrawingMenu == false) {
+    PauseMenuEnabled = !PauseMenuEnabled;
+    if (PauseMenuEnabled == false) {
+      ToggleMusicButton.hide();
+      ToggleSoundButton.hide();
+      ReturnToMenuButton.hide();
+      RestartButton.hide();
+    }
   }
 }
