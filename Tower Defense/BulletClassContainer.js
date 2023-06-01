6daed2aa1a49a03059e35fb0b67e894a4bcc6517
Unknown <88512161+KnownUnknown420@users.noreply.github.com>
2023-06-01 15:16:13 -0700

    Update BulletClassContainer.js

--- Tower Defense/BulletClassContainer.js
+++ Tower Defense/BulletClassContainer.js
@@ -1,59 +1,50 @@
-//This File contains a bullet class.
-//This class is used to create Bullet Particle for all the towers.
-//Towers, like the wizard tower, that require a special animation, get a extended class.
-
-//Bullet class is created
+// Bullet class used to create Bullet Particle for all the towers.
+// Towers, like the wizard tower, that require a special animation, get an extended class.
 class Bullet {
-  //It is constructed with:
+  // Constructs a bullet with a starting position, a target position, and a speed.
   constructor(startX, startY, targetX, targetY, speed) {
-    this.X = startX;
-    this.Y = startY;
-    this.Speed = speed;
-    this.TargetX = targetX;
-    this.TargetY = targetY;
-    this.ReachedTarget = false; //Reached target is for when the bullet reaches it end goal
+    this.x = startX;
+    this.y = startY;
+    this.speed = speed;
+    this.targetX = targetX;
+    this.targetY = targetY;
+    this.reachedTarget = false; // Tracks whether the bullet has reached its target
   }
 
-  //Update is used to change the position of the bullet
-  //Gets the Distance from the bullet and then adds the diistance to the target / Steps
-  //Steps is the Distance / speed. This is used to account for the speed
-  //Otherwise the bullet woud never reach its target
-  //This is because the speed adds a set value to the position everytime (DistanceX / Steps)
+  // Updates the position of the bullet based on its speed.
   update() {
-    if (!this.ReachedTarget) {
-      let DX = this.TargetX - this.X;
-      let DY = this.TargetY - this.Y;
-      let Distance = sqrt(DX * DX + DY * DY);
-      let Steps = Distance / this.Speed;
-      this.X += DX / Steps;
-      this.Y += DY / Steps;
+    if (!this.reachedTarget) {
+      let dX = this.targetX - this.x;
+      let dY = this.targetY - this.y;
+      let distance = sqrt(dX * dX + dY * dY);
+      let steps = distance / this.speed;
+      this.x += dX / steps;
+      this.y += dY / steps;
 
-      //If the bullet has reached its target:
-      if (Distance < this.Speed) {
-        this.ReachedTarget = true; //Saying the bullet as reached its targeta
+      // If the bullet has reached its target, mark it as such
+      if (distance < this.speed) {
+        this.reachedTarget = true;
       }
     }
   }
 
-  //Display is used to show the bullet on the screen
-  //Checks if reachedTarget == false first,
-  //Then draws a bullet, in this case, a point
-  //It also changes the stroke weight to make the bullet more visible to the user
+  // Displays the bullet on the screen as a point.
   display() {
-    if (!this.ReachedTarget) {
+    if (!this.reachedTarget) {
       strokeWeight(5);
-      point(this.X, this.Y);
+      point(this.x, this.y);
       strokeWeight(1);
     }
   }
 }
+
 
 //This is a special bullet type
 //The wizard tower shoots three diffrent "lighting bolts"
 //A line is connected between the Wizard tower's center and the enemy's center
 //I wanted it to be diffrent than a normal bullet, so the LightBolt class was created
 //Counter was made to keep the line on screen for a certin amount of time
-class LightingBolt extends Bullet {
+class LightningBolt extends Bullet {
   constructor(Enemy, X, Y) {
     super();
     this.X = X;
